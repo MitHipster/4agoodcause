29ec52cd3519cd94be301e5fb49bded7c8217fda
Tim Acker <tjacker31@gmail.com>
2017-09-20 22:47:24 -0400

    Table of Contents
    Included missing code to place account page's table of contents in a fixed position when scrolling.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -9,24 +9,28 @@ $(document).ready(function () {
   $('.parallax').parallax();
   // Initialize table of contents structure
   $('.scrollspy').scrollSpy({
-    scrollOffset:0
+    scrollOffset: 64
   });
+  $('.table-of-contents').pushpin({
+      top: 124,
+      offset: 64
+    });
   // Initialize select state dropdown
   $('select').material_select();
   // for HTML5 "required" attribute
   $('select[required]').css({
-      display: 'inline',
-      position: 'absolute',
-      float: 'left',
-      padding: 0,
-      margin: 0,
-      border: '1px solid rgba(255,255,255,0)',
-      height: 0,
-      width: 0,
-      top: '2em',
-      left: '3em',
-      opacity: 0
-    });
+    display: 'inline',
+    position: 'absolute',
+    float: 'left',
+    padding: 0,
+    margin: 0,
+    border: '1px solid rgba(255,255,255,0)',
+    height: 0,
+    width: 0,
+    top: '2em',
+    left: '3em',
+    opacity: 0
+  });
 });
 
 const $categoryForm = $('#category-form');
